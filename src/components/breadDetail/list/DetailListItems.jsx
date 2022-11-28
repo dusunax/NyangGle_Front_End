ee8452b82c13534be9a47bf35bee8d4e4b3725c6
han_kyeul <phk9436phk@gmail.com>
2022-11-29 03:31:41 +0900

    [Feat&Ui] 붕어빵 선택시 READ상태로 변경 기능 구현 및 붕어빵 확인 UI작업

--- src/components/breadDetail/list/DetailListItems.jsx
+++ src/components/breadDetail/list/DetailListItems.jsx
@@ -6,7 +6,7 @@ import styled from 'styled-components';
 import DetailListItem from './DetailListItem';
 
 const BREAD_DATA_ID = {
-  Type: '팥/앙금',
+  Type: '고구마/마라',
   message:
     '안녕 냥냥펀치! 나는 냥아치야 차가운 밀가루 반죽에 아주머니는 연신 허연 입김 불어넣으시고 후끈 달아오른 무쇠틀 속으로 붉은 심장을 넣어주신다 어느새 윤기 흐르는 피부로 세상에 나온 손주들 온기도 가시기 전 봉지에 담긴다 오늘은 아주머니도 월척 붕어 몇 마리 낚아',
   createdAt: '',
@@ -38,9 +38,12 @@ function DetailListItems({ currentIndex, baseUrl, token }) {
     if (isOpened) return;
     setIsOpened(true);
     setReadingId(id);
-    const openedBread = breadList[currentIndex].find((e) => e.id === id);
-    console.log(breadList, breadList[currentIndex], openedBread);
-    setBreadList((state) => [...state, ]);
+    let currentList = [...breadList[currentIndex]].map((e) =>
+      e.id === id ? { ...e, status: 'READ' } : e,
+    );
+    let totalList = [...breadList];
+    totalList[currentIndex] = currentList;
+    setBreadList(totalList);
     const hasReadingData = readingData.find((e) => e.id === id);
     hasReadingData ?? getBreadDetail(id);
   };
