651e4ebc1d455765cf9cf22e7c66f78abb44d890
Dusuna <dusunax@gmail.com>
2022-11-28 22:53:39 +0900

    [Ui] member 페이지 View 작업 및 변경 Ui 코드 반영

--- src/pages/login/Index.jsx
+++ src/pages/login/Index.jsx
@@ -17,7 +17,11 @@ function Login() {
       <div className="contents_area">
         <IntroTitle>냥냥편지</IntroTitle>
         <div className="imageWrap">
-          <img className="catTruck" src="public/assets/images/intro/catTruck.png" alt="Cat Truck" />
+          <img
+            className="catTruck"
+            src="public/assets/images/intro/cat_truck.png"
+            alt="Cat Truck"
+          />
         </div>
         <KakaoLogin onClick={onClickKakaoLoginButton}>
           <KakaoLoginImage
@@ -35,7 +39,7 @@ export default Login;
 const LoginWrap = styled.div`
   height: calc(var(--vh, 1vh) * 100);
 
-  background: linear-gradient(to bottom, #e3edf2 70%, #000 70%, #000 70.3%, #faeac7 70.3%);
+  background: linear-gradient(to bottom, #e3edf2 68%, #000 68%, #000 68.3%, #faeac7 68.3%);
 
   .contents_area {
     height: 100%;
@@ -112,16 +116,14 @@ const IntroTitle = styled.h1`
 const KakaoLogin = styled.button`
   width: 100%;
   max-height: 50px;
+  border-radius: 5px;
+  overflow: hidden;
 
   margin-bottom: 55px;
   padding: 0;
   background-color: transparent;
   border: none;
   cursor: pointer;
-`;
-
-const KakaoLoginImage = styled.img`
-  width: 100%;
 
   transition: all 0.2s;
 
@@ -130,4 +132,8 @@ const KakaoLoginImage = styled.img`
     transform: translateY(-2px) scale(1.01);
     box-shadow: 0px 2px 4px rgba(55, 55, 55, 0.1);
   }
+`;
+
+const KakaoLoginImage = styled.img`
+  width: 100%;
 `;
--- src/pages/member/Member.jsx
+++ src/pages/member/Member.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import * as sizeImage from './sizeImage.json';
+import * as countFishTruckImages from './countFishTruckImages.json';
 
 import styled from 'styled-components';
 
@@ -15,7 +15,7 @@ function Member() {
   const [isEditMode, setIsEditMode] = useState(false);
 
   const [fishSizeAll, setFishSizeAll] = useState();
-  const [displayFishImage, setDisplayFishImage] = useState();
+  const [displayFishImage, setDisplayFishImage] = useState('cat_truck_6.png');
   const [activeHamburger, setActiveHamburger] = useState(false);
 
   // 리코일으로 전역 변수 가져와서 사용
@@ -45,11 +45,20 @@ function Member() {
     const { fetchContentSize } = await useFetchContentSize();
     // {success: boolean / sizeAll: number[] }
     const fetchedContents = await fetchContentSize();
-    const fishCount = fetchedContents.sizeAllCount;
 
-    setDisplayFishImage(sizeImage.default[fishCount - 1].imageURL);
-    setFishSizeAll(fishCount);
+    if (fetchedContents.success) {
+      const fishCount = fetchedContents.sizeAllCount;
+
+      if (fishCount < 6) {
+        setDisplayFishImage(countFishTruckImages.default[fishCount].imageURL);
+      }
+      setFishSizeAll(fishCount);
+    } else {
+      navigate('/');
+    }
   };
+
+  console.log(displayFishImage);
 
   const onSubmit = async (event) => {
     event.preventDefault();
@@ -110,78 +119,94 @@ function Member() {
   }, []);
 
   return (
-    <MemberWrap>
-      <div className="contents_area">
-        {/* 타이틀 */}
-        <SectionTitle>
-          <HamburgerWarp>
-            <div className="hambuger" onClick={onClickHamburgerButton}>
-              🍔
-            </div>
-            <ul className={activeHamburger ? 'hambugerMenu active' : 'hambugerMenu'}>
-              <li onClick={onClickKakaoLogoutButton}>로그아웃</li>
-            </ul>
-          </HamburgerWarp>
-
-          {/* 붕어빵이 n개 있습니다냥 */}
-          <NickNameChangeForm onSubmit={onSubmit} onClick={onClickNickName}>
-            {isEditMode ? (
-              <input
-                className="username"
-                defaultValue={userName}
-                onChange={onChange}
-                maxLength={10}
-              />
-            ) : (
-              <span className="username">{userName}</span>
+    <>
+      <MemberWrap>
+        <div className="contents_area">
+          {/* 타이틀 */}
+          <SectionTitle>
+            {/* 붕어빵이 n개 있습니다냥 */}
+            {isMyPage && (
+              <NickNameChangeForm onSubmit={onSubmit} onClick={onClickNickName}>
+                {isEditMode ? (
+                  <input
+                    className="username"
+                    defaultValue={userName}
+                    onChange={onChange}
+                    maxLength={10}
+                  />
+                ) : (
+                  <span className="username">{userName}!</span>
+                )}
+                <br />
+                붕어빵이 <span className="sizeAll">{fishSizeAll}</span>개 있다냥
+              </NickNameChangeForm>
             )}
-            <br />
-            붕어빵이 <span className="sizeAll">{fishSizeAll}</span>개 있습니다냥.
-          </NickNameChangeForm>
-        </SectionTitle>
 
-        {/* 푸드트럭 이미지 & 붕어빵 매대 */}
-        <FishBreadTruckWrap>
-          {/* url 복사 */}
-          <CopyUrlWrap onClick={copyUrl}>
-            <input id="copyUrl" type="text" ref={copyUrlRef} defaultValue={window.location.href} />
-            <label htmlFor="copyUrl">🔗</label>
-          </CopyUrlWrap>
+            <div className="right">
+              {/* url 복사 */}
+              <CopyUrlWrap onClick={copyUrl}>
+                <input
+                  id="copyUrl"
+                  type="text"
+                  ref={copyUrlRef}
+                  defaultValue={window.location.href}
+                />
+              </CopyUrlWrap>
+              {/* <HamburgerWarp>
+              <div className="hambuger" onClick={onClickHamburgerButton}>
+                🍔
+              </div>
+              <ul className={activeHamburger ? 'hambugerMenu active' : 'hambugerMenu'}>
+                <li onClick={onClickKakaoLogoutButton}>로그아웃</li>
+              </ul>
+            </HamburgerWarp> */}
+            </div>
+          </SectionTitle>
 
-          <FishBreadTruck>
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/4105/4105446.png"
-              alt="임시 이미지"
-              style={{ width: '300px' }}
-            />
+          {/* 푸드트럭 이미지 & 붕어빵 매대 */}
+          <FishBreadTruckWrap>
+            <FishBreadTruck>
+              <img
+                src={`public/assets/images/member/${displayFishImage}`}
+                alt="고양이 트럭이다냥"
+                className={isMyPage ? 'catTruck clickable' : 'catTruck'}
+                onClick={isMyPage ? setPage.bind(this, `/list/${uid}`) : null}
+              />
 
-            <FishBreadConatiner
-              className={isMyPage ? 'clickable' : ''}
-              onClick={isMyPage ? setPage.bind(this, `/list/${uid}`) : null}
-            >
-              <img src={displayFishImage} alt="붕어빵 매대입니다." />
-            </FishBreadConatiner>
-          </FishBreadTruck>
-        </FishBreadTruckWrap>
-        <ButtonConatiner>
-          {isMyPage && <button onClick={setPage.bind(this, `/list/${uid}`)}>내 봉투 가기</button>}
+              {/* <FishBreadConatiner
+                className={isMyPage ? 'clickable' : ''}
+                onClick={isMyPage ? setPage.bind(this, `/list/${uid}`) : null}
+              >
+                <img src={displayFishImage} alt="붕어빵 매대입니다." />
+              </FishBreadConatiner> */}
+            </FishBreadTruck>
+          </FishBreadTruckWrap>
+          <ButtonConatiner>
+            {isMyPage && (
+              <button onClick={setPage.bind(this, `/list/${uid}`)}>내가 받은 붕어빵 확인</button>
+            )}
 
-          {isLoggedUser && !isMatchUid && (
-            <>
-              <button onClick={setPage.bind(this, `/member/${myUid}`)}>내 트럭 가기</button>
-              <button onClick={setPage.bind(this, `/customFish/${uid}`)}>붕어빵 만들기</button>
-            </>
-          )}
+            {isLoggedUser && !isMatchUid && (
+              <>
+                <button onClick={setPage.bind(this, `/member/${myUid}`)}>
+                  내 붕어빵 페이지 보기{' '}
+                </button>
+                <button onClick={setPage.bind(this, `/customFish/${uid}`)}>붕어빵 만들기</button>
+              </>
+            )}
 
-          {!isLoggedUser && (
-            <>
-              <button onClick={setPage.bind(this, `/customFish/${uid}`)}>붕어빵 만들기</button>
-              <button onClick={setPage.bind(this, `/`)}>내 봉투 만들기</button>
-            </>
-          )}
-        </ButtonConatiner>
-      </div>
-    </MemberWrap>
+            {!isLoggedUser && (
+              <>
+                <button onClick={setPage.bind(this, `/customFish/${uid}`)}>붕어빵 만들기</button>
+                <button onClick={setPage.bind(this, `/`)} className="buttonLink">
+                  <span>로그인 하러 가기</span>
+                </button>
+              </>
+            )}
+          </ButtonConatiner>
+        </div>
+      </MemberWrap>
+    </>
   );
 }
 
@@ -190,7 +215,7 @@ export default Member;
 const MemberWrap = styled.div`
   height: calc(var(--vh, 1vh) * 100);
 
-  background: linear-gradient(to bottom, #e3edf2 70%, #000 70%, #000 70.3%, #faeac7 70.3%);
+  background: linear-gradient(to bottom, #e3edf2 68%, #000 68%, #000 68.3%, #faeac7 68.3%);
 
   .contents_area {
     height: 100%;
@@ -208,46 +233,55 @@ const MemberWrap = styled.div`
 `;
 
 const SectionTitle = styled.section`
+  /* margin-bottom: 5vh; */
+  min-height: 20%;
   display: flex;
-  flex-flow: column;
-`;
-
-const NickNameChangeForm = styled.form`
-  /* padding-top: 40px; */
-  margin-bottom: 10vh;
-
-  font-family: 'EF_jejudoldam';
-  text-align: center;
-  font-size: 22px;
-  line-height: 1.8;
 
   position: relative;
 
+  .right {
+    flex: 1;
+  }
+`;
+
+const NickNameChangeForm = styled.form`
+  flex: 1;
+  /* margin-bottom: 10vh; */
+
+  font-family: 'EF_jejudoldam';
+  font-size: 30px;
+  line-height: 1.3;
+  word-break: keep-all;
+
+  position: relative;
+  z-index: 99;
+
+  text-shadow: -1px 0 #e3edf2, 0 1px #e3edf2, 1px 0 #e3edf2, 0 -1px #e3edf2;
+
   @media (max-width: 500px) {
-    font-size: 20px;
+    font-size: 28px;
   }
   @media (max-width: 400px) {
-    font-size: 16px;
+    font-size: 26px;
   }
   @media (max-width: 300px) {
-    font-size: 14px;
+    font-size: 18px;
   }
 
   .username {
     width: 100%;
     display: block;
-    text-align: center;
 
     padding: 5px 0;
+    white-space: nowrap;
 
     position: absolute;
-    top: 0;
+    top: -7px;
     left: 50%;
     transform: translateX(-50%);
   }
 
   input.username {
-    width: 70%;
     font-family: 'EF_jejudoldam';
 
     outline: none;
@@ -255,9 +289,7 @@ const NickNameChangeForm = styled.form`
     border-bottom: 2px solid #b5cfe9;
 
     position: absolute;
-    top: 8px;
-    left: 50%;
-    transform: translateX(-50%);
+    top: 0px;
 
     word-break: keep-all;
     white-space: nowrap;
@@ -299,7 +331,6 @@ const NickNameChangeForm = styled.form`
 // 햄버거 메뉴
 const HamburgerWarp = styled.div`
   position: relative;
-  align-self: flex-end;
 
   font-size: 24px;
 
@@ -344,33 +375,37 @@ const HamburgerWarp = styled.div`
 
 // 트럭 시작
 const FishBreadTruckWrap = styled.div`
-  height: 50vh;
+  width: 100%;
+  height: 50%;
   position: relative;
 
-  margin-bottom: 5vh;
-
-  display: flex;
-  flex-flow: column;
-  align-items: center;
-  justify-content: center;
-
-  background-color: #fff;
+  margin-bottom: 2%;
 `;
 
 const FishBreadTruck = styled.div`
-  position: relative;
+  background-color: #fff;
 
-  margin: 40px 0;
+  .catTruck {
+    height: 100%;
+    max-width: 100%;
+    object-fit: contain;
+
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+  }
 `;
 
 const FishBreadConatiner = styled.ul`
   display: flex;
-  width: 100px;
+  width: 125px;
+  height: 80px;
   flex-flow: wrap;
 
   position: absolute;
-  right: 0;
-  bottom: 150px;
+  left: 40px;
+  bottom: 165px;
 
   &.clickable {
     cursor: pointer;
@@ -381,42 +416,102 @@ const FishBreadConatiner = styled.ul`
   }
 `;
 
+// 링크 복사 버튼
 const CopyUrlWrap = styled.div`
-  padding: 10px 20px;
-  border-radius: 30px;
-  border: 1px solid #ddd;
+  width: 66px;
+  height: 36px;
+  margin-left: auto;
 
-  display: flex;
-  align-items: center;
-  gap: 10px;
+  position: relative;
+  right: 0;
+  top: 0;
+
+  background: url('./assets/images/member/link_button.png') no-repeat center / contain;
 
   cursor: pointer;
 
-  input {
-    all: unset;
-    width: 280px;
-    font-size: 18px;
+  transition: all 0.3s;
 
-    overflow: hidden;
-    white-space: nowrap;
-    word-break: keep-all;
+  @media (max-width: 500px) {
+    width: 60px;
+  }
+  @media (max-width: 400px) {
+    width: 55px;
+  }
+  @media (max-width: 300px) {
+    width: 50px;
+  }
 
-    text-overflow: ellipsis;
+  animation: swing 1s 0.5s infinite;
+
+  @keyframes swing {
+    0% {
+      transform: rotate(0);
+    }
+    30% {
+      transform: rotate(-5deg);
+    }
+    60% {
+      transform: rotate(5deg);
+    }
+    90% {
+      transform: rotate(0);
+    }
+  }
+
+  &:hover {
+    transform: translateY(-3px) rotate(0);
+  }
+
+  #copyUrl {
+    display: block;
+    height: 0;
+    width: 0;
+    position: absolute;
+    z-index: -9;
+    border: none;
+    padding: 0;
   }
 `;
 
+// 버튼 박스
 const ButtonConatiner = styled.div`
-  /* background-color: tan; */
   button {
-    font-family: 'EF_jejudoldam';
-
     width: 100%;
-    max-height: 50px;
+    height: 70px;
 
     padding: 0;
     background-color: transparent;
     border: none;
     cursor: pointer;
+
+    font-size: 18px;
+    line-height: 28px;
+    font-weight: 700;
+
+    color: #ffffff;
+    background: url('./assets/images/member/button.png') no-repeat center / contain;
+
+    transition: all 0.2s;
+
+    &:hover {
+      transform: translateY(-2px);
+      box-shadow: 0px 2px 4px rgba(55, 55, 55, 0.1);
+    }
   }
-  margin-bottom: 55px;
+  .buttonLink {
+    height: 50px;
+    background: transparent;
+    color: #73390b;
+    span {
+      display: inline-block;
+      line-height: 28px;
+      border-bottom: 2px solid #73390b;
+    }
+    &:hover {
+      transform: translateY(-1px);
+      opacity: 0.8;
+      box-shadow: none;
+    }
+  }
 `;
