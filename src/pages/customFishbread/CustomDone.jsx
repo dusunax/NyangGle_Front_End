808490a5ad879bb985b12cd7721505d80948b983
trondi <trond746@gmail.com>
2022-11-29 23:04:27 +0900

    [Hotfix] uid추가

--- src/pages/customFishbread/CustomDone.jsx
+++ src/pages/customFishbread/CustomDone.jsx
@@ -38,7 +38,9 @@ function CustomDone({ dough }) {
   return (
     <div>
       <Header>
-        <p className="message">노릇노릇해지고 있다냥</p>
+        <ContentsArea>
+          <p className="message">노릇노릇해지고 있다냥</p>
+        </ContentsArea>
       </Header>
       <Main>
         <Contents>
@@ -59,8 +61,6 @@ function CustomDone({ dough }) {
               id="dough_prev"
             />
           </Fish>
-
-          <img src="/assets/custommessage/frame.svg" className="fishFrame" />
         </FishFrame>
       </Main>
     </div>
@@ -69,22 +69,26 @@ function CustomDone({ dough }) {
 
 export default CustomDone;
 
+const ContentsArea = styled.div`
+  padding: 0 18px;
+`;
+
 const Fish = styled.div`
   ${({ theme }) => theme.flex.col}
   align-items: center;
-  justify-content: center;
-
   .dough_prev {
     position: absolute;
-    transform: translate(5px, 31vh);
+    right: 44.5%;
+    top: 65%;
     opacity: 1;
     animation: fadeout 2s 0.2s forwards;
     z-index: 9;
   }
   .dough_next {
     position: absolute;
-    transform: translate(5px, 31vh);
-    opacity: 1;
+    right: 44.5%;
+    top: 65%;
+    opacity: 0;
     animation: fadeIn 2s 0.2s forwards;
     z-index: 9;
   }
@@ -107,47 +111,61 @@ const Fish = styled.div`
 `;
 
 const FishFrame = styled.section`
-  flex: 1;
   width: 100%;
-  background: linear-gradient(#fff, #8c8c8c);
-
+  height: 60vh;
+  /* background: linear-gradient(transparent, #8c8c8c); */
+  background: url('./assets/customfish/fishframe_wide.png') no-repeat top center / 110%,
+    linear-gradient(transparent 40%, #9e9e9e 40%);
+  z-index: 9;
   .fishFrame {
-    position: absolute;
-    width: 768px;
-    transform: translate(0, 110px);
+    bottom: 0;
+    margin-bottom: 10vh;
   }
 `;
 
 const Main = styled.main`
   ${({ theme }) => theme.flex.col}
+  height: 100vh;
+  justify-content: space-between;
 `;
 
 const Header = styled.header`
   padding: 20px;
-
   .btns {
     ${({ theme }) => theme.flex.row}
     align-items: center;
     justify-content: space-between;
   }
-
   .message {
     margin: 30px 0;
     padding: 30px;
     background-color: #eee;
-    border-radius: 15px;
+    border-radius: 14px;
     text-align: center;
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 28px;
+    word-break: keep-all;
   }
 `;
 
 const Contents = styled.section`
   flex: 1;
   ${({ theme }) => theme.flex.col}
+  justify-content: flex-end;
   align-items: center;
   position: relative;
-
   .cat {
-    width: 150px;
+    width: 50%;
+    max-width: 188px;
     position: absolute;
+    top: 280px;
+    @media (max-width: 400px) {
+      width: 40%;
+      top: -100px;
+    }
+    @media (max-width: 350px) {
+      top: -80px;
+    }
   }
 `;
