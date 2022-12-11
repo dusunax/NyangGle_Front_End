81475e2f76e3465125be30f7128e1249a201e750
trondi <trond746@gmail.com>
2022-12-11 20:08:50 +0900

    [UI] customMessage 컴포넌트 삭제 및 반응형 수정

--- src/pages/customFishbread/CustomMessage.jsx
+++ src/pages/customFishbread/CustomMessage.jsx
@@ -36,34 +36,29 @@ function CustomMessage({ inputs, onChangeMessage, onClickedSave }) {
   }, []);
 
   return (
-    <Wrapper style={{ display: 'flex', position: 'relative' }}>
+    <Wrapper>
       <Paper
         style={{
           backgroundImage: `url("/assets/custommessage/${imgs}.svg")`,
         }}
       >
-        <Content>
-          <ReceiveNickname>
-            <input className="receiveNickname" name="receiveNickname" value="sooya" disabled />
-          </ReceiveNickname>
-          <Textbox
-            name="message"
-            onChange={onChangeMessage}
-            value={inputs.message}
-            maxLength="500"
-            placeholder="내용을 입력하세요"
-          />
-          <SenderNickname>
-            <input
-              className="senderNickname"
-              name="senderNickname"
-              onChange={onChangeMessage}
-              placeholder="익명"
-              value={inputs.senderNickname}
-              maxLength="6"
-            />
-          </SenderNickname>
-        </Content>
+        <input className="receiveNickname" name="receiveNickname" value="sooya" disabled />
+        <textarea
+          name="message"
+          className="message"
+          onChange={onChangeMessage}
+          value={inputs.message}
+          maxLength="500"
+          placeholder="내용을 입력하세요"
+        />
+        <input
+          className="senderNickname"
+          name="senderNickname"
+          onChange={onChangeMessage}
+          placeholder="익명"
+          value={inputs.senderNickname}
+          maxLength="6"
+        />
       </Paper>
     </Wrapper>
   );
@@ -76,7 +71,6 @@ const Wrapper = styled.section`
 
   display: flex;
   position: relative;
-  /* position: absolute; */
   left: 0;
   bottom: 0;
 
@@ -90,44 +84,40 @@ const Wrapper = styled.section`
   @media (max-width: 300px) {
     bottom: -40px;
   }
-
-  /* @media (max-height: 900px) {
-    bottom: -20px;
-  }
-  @media (max-height: 700px) {
-    bottom: -30px;
-  } */
 `;
 
 const Paper = styled.div`
-  width: 600px;
-  height: 800px;
-  z-index: 3;
-  object-fit: cover;
-
-  background-size: cover;
+  width: 350px;
+  height: 490px;
   display: flex;
-  margin-top: -70px;
+  justify-content: space-between;
+  padding: 10% 10%;
+  flex-direction: column;
+  position: relative;
+  font-size: 20px;
+  background-size: cover;
+
+  margin-top: -15%;
   text-align: center;
 
-  @media (max-width: 700px) {
-    width: 550px;
-    height: 750px;
+  @media (max-width: 999px) {
+    width: 470px;
+    height: 650px;
   }
   @media (max-width: 600px) {
-    width: 450px;
+    width: 460px;
     height: 650px;
   }
   @media (max-width: 450px) {
-    width: 350px;
-    height: 500px;
-    @media (max-height: 900px) {
-      width: 370px;
-      height: 500px;
-    }
-    @media (max-height: 1000px) {
-      width: 380px;
-      height: 500px;
+    width: 400px;
+    height: 550px;
+  }
+
+  @media (max-width: 400px) {
+    width: 380px;
+    height: 550px;
+    @media (min-height: 800px) {
+      height: 530px;
     }
   }
   @media (max-width: 370px) {
@@ -138,238 +128,50 @@ const Paper = styled.div`
     width: 300px;
     height: 400px;
   }
-`;
-
-const Content = styled.div`
-  margin: 80px 20px;
-  display: flex;
-  flex-direction: row;
-
-  /* position: absolute; */
-  flex-wrap: wrap;
-  /* z-index: 1; */
-
-  input {
-    background: transparent;
-    border: none;
-    height: 50px;
-
-    /* outline: none; */
-    font-size: 30px;
-    position: absolute;
-  }
-
-  @media (max-width: 600px) {
-    margin: 0;
-  }
-  @media (max-width: 400px) {
-    /* width: 100%; */
-    height: 80%;
-
-    input {
-      position: fixed;
-      font-size: 25px;
-      height: 30px;
-    }
-  }
-  @media (max-width: 350px) {
-    bottom: 10px;
-  }
-`;
-
-const Textbox = styled.textarea`
-  width: 90%;
-  height: 70%;
-  resize: none;
-  background: none;
-  border: none;
-  font-size: 35px;
-  position: absolute;
-  top: 10%;
-  left: 5%;
-
-  margin-top: 10px;
-
-  /* @media (max-width: 1200px) {
-    /* top: 50px; */
-  /* height: 540px;
-  } */
-  @media (max-width: 600px) {
-    top: 3%;
-    font-size: 20px;
-  }
-
-  @media (max-width: 450px) {
-    left: 5%;
-  }
-  @media (max-width: 400px) {
-    width: 85%;
-    height: 70%;
-    top: -2%;
-    font-size: 20px;
-
-    @media (min-height: 800px) {
-      height: 80%;
-    }
-    @media (min-height: 900px) {
-      font-size: 20px;
-    }
-  }
-  /* @media (max-width: 350px) {
-    top: 10px;
-    top: -80px;
-  } */
-`;
-
-const ReceiveNickname = styled.div`
-  position: absolute;
 
   .receiveNickname {
-    position: fixed;
-    top: 30%;
-    left: 43%;
-    width: 210px;
-    font-size: 30px;
+    line-height: 10px;
+    background: none;
+    border: none;
+    padding-left: 50px;
+    align-items: flex-start;
+    font-size: 25px;
   }
 
-  @media (max-width: 1500px) {
-    .receiveNickname {
-      left: 41%;
-    }
+  .message {
+    border: none;
+    font-size: 25px;
+    resize: none;
+    width: 95%;
+    height: 75%;
+    background: none;
   }
-  @media (max-width: 1200px) {
-    .receiveNickname {
-      left: 38%;
-    }
-  }
-  @media (max-width: 900px) {
-    .receiveNickname {
-      left: 35%;
-    }
-  }
-  @media (max-width: 600px) {
-    .receiveNickname {
-      top: 32%;
-      left: 32%;
-      font-size: 20px;
-    }
-  }
-  @media (max-width: 450px) {
-    .receiveNickname {
-      top: 35%;
-      left: 25%;
-      font-size: 20px;
-    }
-  }
-  @media (max-width: 400px) {
-    .receiveNickname {
-      top: 42%;
-      left: 27%;
-    }
-    @media (min-height: 700px) {
-      .receiveNickname {
-        top: 43%;
-        left: 30%;
-      }
-    }
-    @media (min-height: 800px) {
-      .receiveNickname {
-        top: 37%;
-        left: 25%;
-      }
-    }
-  }
-`;
-
-const SenderNickname = styled.div`
-  /* overflow: hidden; */
-  /* float: left; */
-  position: absolute;
 
   .senderNickname {
-    position: fixed;
-    /* float: right; */
-    /* transition: right; */
-    width: 210px;
-    left: 55%;
-    bottom: 13%;
-  }
-
-  @media (max-width: 1400px) {
-    .senderNickname {
-      left: 60%;
-      bottom: 12%;
-    }
-  }
-  @media (max-width: 900px) {
-    .senderNickname {
-      left: 65%;
-    }
-  }
-  @media (max-width: 700px) {
-    .senderNickname {
-      left: 63%;
-      bottom: 10%;
-      font-size: 20px;
-    }
-  }
-  @media (max-width: 600px) {
-    .senderNickname {
-      left: 65%;
-      bottom: 23%;
-    }
-  }
-  @media (max-width: 450px) {
-    .senderNickname {
-      left: 65%;
-      bottom: 23%;
-    }
-    @media (min-height: 800px) {
-      .senderNickname {
-        left: 65%;
-        bottom: 15%;
-      }
-    }
-    @media (min-height: 900px) {
-      .senderNickname {
-        left: 65%;
-        bottom: 17%;
-      }
-    }
-    @media (min-height: 1000px) {
-      .senderNickname {
-        left: 65%;
-        bottom: 15%;
-      }
-    }
-  }
-  @media (max-width: 400px) {
-    .senderNickname {
-      left: 62%;
-      bottom: 9%;
-      /* left: 220px;
-      width: 300px;
-      bottom: 50px; */
-      font-size: 20px;
-    }
+    border: none;
+    position: relative;
+    font-size: 25px;
+    align-self: flex-end;
+    background: none;
+    width: 100px;
+    left: 2%;
+    bottom: 10%;
 
     @media (min-height: 600px) {
-      .senderNickname {
-        left: 65%;
-        bottom: 8%;
-      }
+      top: -3%;
+      left: 10%;
     }
-    @media (min-height: 800px) {
-      .senderNickname {
-        left: 65%;
-        bottom: 13%;
-      }
+    @media (min-height: 700px) {
+      top: 0%;
+      left: 10%;
+    }
+    @media (min-height: 700px) {
+      top: -5%;
+      left: 10%;
     }
     @media (min-height: 900px) {
-      .senderNickname {
-        left: 65%;
-        bottom: 15%;
-      }
+      top: -5%;
+      left: 10%;
     }
   }
 `;
